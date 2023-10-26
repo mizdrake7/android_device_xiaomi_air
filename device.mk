1d3640d43a23d190f5f7008a6cbc952a3e23f7d4
Yifan Hong <elsk@google.com>
2023-10-26 09:36:54 +0530

    air: Enforce generic ramdisk allow list.
    his change enforces that only the list of files on the generic
    ramdisk can be installed to the ramdisk of this device.
    
    This is required for all devices using generic ramdisk.
    
    Test: m nothing
    Bug: 173742069
    Change-Id: Icff7f30b5aef9fdc59b3060316576f1f50b2a92c

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # A/B
 PRODUCT_PACKAGES += \
     update_engine \
