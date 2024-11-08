5fd823c6685187a1f6c1de0f55b2bff5c402c661
MAdMiZ <mizdrake7@gmail.com>
2024-11-08 21:43:58 +0700

    air: Drop obsolete PRODUCT_STATIC_BOOT_CONTROL_HAL
    - recovery/xiaomi/air/device.mk:24: error: PRODUCT_STATIC_BOOT_CONTROL_HAL is obsolete. Use shared library module instead. See https://android.googlesource.com/platform/build/+/master/Changes.md#PRODUCT_STATIC_BOOT_CONTROL_HAL.
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -16,16 +16,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl.recovery
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6835
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6835 \
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl.mt6835.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
