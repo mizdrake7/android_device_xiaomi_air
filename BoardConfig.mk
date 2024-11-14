915cdd5fcd05a5fd6a6b2117e3d0405643fecf65
Koushik Dey <kousickdey3@gmail.com>
2024-11-14 14:23:16 +0000

    air: Initial TWRP skeleton tree
    Made with SebaUbuntu's twrpdtgen
    Arch: arm64
    Manufacturer: xiaomi
    Model: air
    Script version: 3.0.0
    
    Change-Id: I605fc3844d5cf103c60a8f3616a94fed3a6c0048

---
+++ BoardConfig.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/air
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Display
+TARGET_SCREEN_DENSITY := 320
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6835
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-08-01
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+TW_INCLUDE_REPACKTOOLS := true
---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/xiaomi/air
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
