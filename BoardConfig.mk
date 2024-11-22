dbc9628b2983322f70b1b495046177e02f5e9d75
Koushik Dey <kousickdey3@gmail.com>
2024-11-22 12:31:48 +0530

    air: Set `BOARD_VNDK_VERSION`
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Change-Id: I62c8cc4e32fc302b8ec9bdc148893222bd332a0c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
+# Treble
+BOARD_VNDK_VERSION := current
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
