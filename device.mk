a9f1b0944e6ac467f4029a4efbeb1ba8d3fb2b1b
MAdMiZ <mizdrake7@gmail.com>
2024-11-28 20:58:06 +0000

    air: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -5,7 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/xiaomi/air
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 32
