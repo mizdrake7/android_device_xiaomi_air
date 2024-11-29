87ab34be07a5d1ed76187c94a26aa36573693888
Kuba Wojciechowski <nullbytepl@gmail.com>
2024-11-29 18:22:50 +0000

    air: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Ica465f586a44129047c266597316045fab762550
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,8 @@ BOARD_RAMDISK_USE_LZ4 := true
 BOARD_KERNEL_CMDLINE += \
     bootopt=64S3,32N2,64N2 \
     androidboot.selinux=permissive \
-    androidboot.init_fatal_reboot_target=recovery
+    androidboot.init_fatal_reboot_target=recovery \
+    kpti=off
 
 BOARD_MKBOOTIMG_ARGS += \
     --header_version $(BOARD_BOOT_HEADER_VERSION) \
